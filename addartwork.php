672951b6801f8528888a1b315e1e6e4406405005
Belinda <belinda.a.williams@hotmail.co.uk>
2021-09-29 15:41:48 +0100

    styles added to form and buttons

--- addartwork.php
+++ addartwork.php
@@ -1,7 +1,3 @@
-<?php
-//require_once 'db.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>Add Artwork | artGallery</title>
 </head>
 
@@ -23,7 +20,13 @@
     <h2>ADD ARTWORKS</h2>
 </header>
 
-<main>
+<form action="index.php">
+    <button class="back">
+        <i class="fas fa-chevron-circle-left"></i>
+    </button>
+</form>
+
+<main class="form">
 
     <?php
     require_once('form.php');
@@ -31,7 +34,7 @@
 
 </main>
 
-<footer class="footer">
+<footer class="footer bottom">
     <p>&#169; artGallery 2021</p>
 </footer>
 
--- form.php
+++ form.php
@@ -1,33 +1,37 @@
 
 <form action="newart.php" method="POST">
 
-    <p>
-        <label for="artist">Artist*:</label>
-        <input type="text" name="artist" id="artist" minlength="1" maxlength="255" aria-required="true">
-    </p>
+    <div class="text-fields">
+        <p>
+            <label for="artist">Artist*:</label>
+            <input type="text" name="artist" id="artist" minlength="1" maxlength="255" aria-required="true">
+        </p>
 
-    <p>
-        <label for="title">Title*:</label>
-        <input type="text" name="title" id="title" minlength="1" maxlength="255" aria-required="true">
-    </p>
+        <p>
+            <label for="title">Title*:</label>
+            <input type="text" name="title" id="title" minlength="1" maxlength="255" aria-required="true">
+        </p>
 
-    <p>
-        <label for="medium">Medium:</label>
-        <input type="text" name="medium" id="medium" maxlength="255">
-    </p>
+        <p>
+            <label for="medium">Medium:</label>
+            <input type="text" name="medium" id="medium" maxlength="255">
+        </p>
 
-    <p>
-        <label for="size">Size:</label>
-        <input type="text" name="size" id="size" maxlength="255">
-    </p>
+        <p>
+            <label for="size">Size:</label>
+            <input type="text" name="size" id="size" maxlength="255">
+        </p>
 
-    <p>
-        <label for="image">Image File Name:</label>
-        <input type="text" name="image" id="image" maxlength="255">
-    </p>
+        <p>
+            <label for="image">Image File Name:</label>
+            <input type="text" name="image" id="image" maxlength="255">
+        </p>
+    </div>
 
-    <input type="reset" value="Refresh">
+    <div class="form-buttons">
+        <input class="refresh" type="reset" value="Refresh">
 
-    <input type="submit" value="Save and Return">
+        <input class="save" type="submit" value="Save and Return">
+    </div>
 
 </form>
